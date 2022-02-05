af1b33051cf5d98c62f42202f39384fca441ada0
kang <linmingkang2020@163.com>
2022-02-05 17:56:23 +0800

    第一版本提交

---
+++ app/src/main/java/com/example/filepersistencetest/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.filepersistencetest
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import java.io.*
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val inputText = load()
+        val editText1:EditText = findViewById(R.id.editText)
+        if (inputText.isNotEmpty()) {
+            editText1.setText(inputText)
+            editText1.setSelection(inputText.length)
+            Toast.makeText(this, "Restoring successed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val editText1:EditText = findViewById(R.id.editText)
+        val inputText = editText1.text.toString()
+        save(inputText)
+    }
+
+    private fun save(inputText: String) {
+        try {
+            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+            writer.use {
+                it.write(inputText)
+            }
+        }catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun load():String {
+        val content = StringBuilder()
+        try {
+            val input = openFileInput("data")
+            val reader = BufferedReader(InputStreamReader(input))
+            reader.use {
+                reader.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return content.toString()
+    }
+}
